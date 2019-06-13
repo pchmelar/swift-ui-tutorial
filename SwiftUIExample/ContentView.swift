39ed7d985d92e6d33654d0bbf09e58ae37b72e63
filletzz <filletzz@gmail.com>
2019-06-13 22:29:54 +0200

    Mismatched types in body

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -9,15 +9,28 @@
 import SwiftUI
 
 struct ContentView : View {
+	
+	// What if we have to return mismatched types?
+	
+	// First approach, worse readability but better performance
     var body: some View {
-		ZStack() {
-			Image("mate")
-			Text("CLUB MATE")
-				.font(.largeTitle)
-				.background(Color.black)
-				.foregroundColor(.yellow)
+		Group {
+			if Bool.random() {
+				Image("mate")
+			} else {
+				Text("Try again :(")
+			}
 		}
     }
+	
+	// Second approach, better readability but worse performance
+//	var body: AnyView {
+//		if Bool.random() {
+//			return AnyView(Image("mate"))
+//		} else {
+//			return AnyView(Text("Better luck next time"))
+//		}
+//	}
 }
 
 #if DEBUG
