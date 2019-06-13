a27cb71127c7effe2a8a590b8a605d3721407cb1
filletzz <filletzz@gmail.com>
2019-06-14 00:03:13 +0200

    SegmentedControl

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -10,23 +10,19 @@ import SwiftUI
 
 struct ContentView : View {
 	
-	var dateFormatter: DateFormatter {
-		let formatter = DateFormatter()
-		formatter.dateStyle = .long
-		return formatter
-	}
+	var colors = ["Red", "Green", "Blue"]
 	
-	@State var birthDate = Date()
+	@State private var favoriteColor = 0
 	
 	var body: some View {
 		VStack {
-			DatePicker(
-				$birthDate,
-				maximumDate: Date(),
-				displayedComponents: .date
-			)
+			SegmentedControl(selection: $favoriteColor) {
+				ForEach(0 ..< colors.count) { index in
+					Text(self.colors[index])
+				}
+			}
 			
-			Text("Date is \(birthDate, formatter: dateFormatter)")
+			Text("Value: \(colors[favoriteColor])")
 		}
 	}
 }
