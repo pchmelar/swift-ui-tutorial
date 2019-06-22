08bfc62b4404fa00238741ceceaf2c02fc739443
filletzz <filletzz@gmail.com>
2019-06-22 18:03:59 +0200

    NavBar show detail view

--- SwiftUIExample/ContentView.swift
+++ SwiftUIExample/ContentView.swift
@@ -9,22 +9,12 @@
 import SwiftUI
 
 struct ContentView : View {
-	
-	@State var showingSheet = false
-	
-	var sheet: ActionSheet {
-		ActionSheet(title: Text("ActionSheet"),
-					message: Text("Available actions below"),
-					buttons: [.default(Text("Action"), onTrigger: { self.showingSheet = false })])
-	}
-	
 	var body: some View {
-		Button(action: {
-			self.showingSheet = true
-		}) {
-			Text("Present action sheet")
+		NavigationView {
+			NavigationButton(destination: DetailView()) {
+				Text("Go to detail")
+			}.navigationBarTitle(Text("List"))
 		}
-		.presentation(showingSheet ? sheet : nil)
 	}
 }
 
---
+++ SwiftUIExample/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  SwiftUIExample
+//
+//  Created by Petr Chmelar on 22/06/2019.
+//  Copyright © 2019 Qest. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView : View {
+	var body: some View {
+		Text("Detail")
+	}
+}
+
+#if DEBUG
+struct DetailView_Previews : PreviewProvider {
+	static var previews: some View {
+		DetailView()
+	}
+}
+#endif
